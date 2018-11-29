4ccb059b60b8b75ed8fe87a98127e26937ece0ef
Marwen trabelsi <marwen.projob@gmail.com>
2018-11-29 14:50:52 +0100

    added dynamic props

--- src/components/ci/Header.js
+++ src/components/ci/Header.js
@@ -13,11 +13,7 @@ class Header extends React.Component {
       <div className="top-header__background">
       </div>
       <div className="top-header__items">
-        <span className="top-header__item">IOTA.ORG</span>
-        <span className="top-header__item">NEWS</span>
-        <span className="top-header__item">WALLET</span>
-        <span className="top-header__item">QUBIC</span>
-        <span className="top-header__item">ECOSYSTEM</span>
+        {this.props.topTitles.map(topTitle => (<span className="top-header__item">{topTitle}</span>))}
       </div>
   </div>
   <div className="header__wrapper">
@@ -26,7 +22,7 @@ class Header extends React.Component {
       <div className="header__icon"><i className="fas fa-bars fa-2x"></i></div>
     </section>
     <section className="header__body">
-      <span className="text text--level1 text--secondary">Developer Documentation</span>
+      <span className="text text--level1 text--secondary">{this.props.headerTitle}</span>
       <div className="header__search">
         <div className="input-wrapper">
           <input type="text" className="input-search" placeholder="Search for topics" />
