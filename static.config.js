b264ec1de8cb488afbef94170d22903ad1c5f206
mtrabelsi <marwen.projob@gmail.com>
2018-11-30 15:53:07 +0100

    added css links

--- static.config.js
+++ static.config.js
@@ -92,14 +92,14 @@ export default {
               content="width=device-width, initial-scale=1"
             />
             <link
-              href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i"
+              href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700%7CMaterial+Icons"
               rel="stylesheet"
             />
             <link
               rel="stylesheet"
               href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
               integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
-              crossorigin="anonymous"
+              crossOrigin="anonymous"
             />
             {renderMeta.styleTags}
             <title>{repoName}</title>
