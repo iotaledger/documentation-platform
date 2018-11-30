86d81cb99d3838a448bad5d756b41897efc6c4e6
mtrabelsi <marwen.projob@gmail.com>
2018-11-30 15:49:17 +0100

    fixed name of dependency

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Routes from "react-static-routes";
 
 import "nprogress/nprogress.css";
 import "react-smackdown/themes/smackdown-light.css";
-import "css-styleguid";
+import "css-styleguide";
 
 import jsx from "reprism/languages/jsx";
 import bash from "reprism/languages/bash";
