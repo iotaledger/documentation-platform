2e187b3055adb7bf68bdacee08ed5526df363641
Alexey Sobolev <sobolev@mail.de>
2018-12-05 13:41:25 +0100

    Removed unused import

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components'
 import Link from '../../atoms/Link';
 import Text from '../../atoms/Text';
 import Heading from '../../atoms/Heading';
