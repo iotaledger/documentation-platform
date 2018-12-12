16d7693f7358ccc1528e0005ebd36c6f8697dd70
Martyn Janes <martyn@obany.com>
2018-12-12 09:06:57 +0300

    Fixed for floating menu display

--- src/components/ci/FloatingMenu.js
+++ src/components/ci/FloatingMenu.js
@@ -1,6 +1,4 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import logo from './../../assets/Logo.svg'
+import React from 'react';
 
 class FloatingMenu extends React.Component {
   constructor(props) {
@@ -9,19 +7,17 @@ class FloatingMenu extends React.Component {
 
   render() {
     return (<ul className="floating-menu" style={this.props.styles}>
-    {Object.values(this.props.data).map(({ name, versions }) => (
-      <li
-        className="floating-menu__item"
-        style={{ cursor: 'pointer' }}
-        key={name}
-        to={{ state: { project: name }}}
-        onClick={(e) => window.open(`/docs/${name}/reference/${Object.keys(versions)[Object.keys(versions).length - 1]}/README`, '_blank') }
-      >
-        {name}
-      </li>)
-    )}
+      {Object.values(this.props.data).map(({ name, versions }) => (
+        <li
+          className="floating-menu__item"
+          key={name}
+          to={{ state: { project: name } }}
+        >
+          <a onClick={(e) => window.open(`/docs/${name}/reference/${Object.keys(versions)[Object.keys(versions).length - 1]}/README`, '_blank')}>{name}</a>
+        </li>)
+      )}
 
-</ul>)
+    </ul>)
   }
 }
- export default FloatingMenu
+export default FloatingMenu
