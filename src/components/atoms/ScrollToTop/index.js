b77a899b67844909696f23e955167776c59ad493
Alexey Sobolev <sobolev@mail.de>
2018-12-12 17:46:17 +0100

    Added scroll-to-top

---
+++ src/components/atoms/ScrollToTop/index.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import button from './../../../assets/back-to-top.svg'
+
+export default class Sticky extends PureComponent {
+  static defaultProps = {
+    top: 350,
+  };
+
+  constructor(props) {
+    super(props);
+    this.setSticky = this.setSticky.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.setSticky);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.setSticky);
+  }
+
+  onScrollToTop() {
+    const target = document.querySelector('#main');
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  setSticky() {
+    const { elem } = this;
+    const { top } = this.props;
+    window.requestAnimationFrame(() => {
+      const scrollTop =
+        window.pageYOffset !== undefined
+          ? window.pageYOffset
+          : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop >= top) {
+        elem.style.position = 'fixed';
+        elem.style.bottom = '130px';
+      } else {
+        elem.style.position = 'relative';
+        elem.style.bottom = 'unset';
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="scroll-to-top" ref={elem => (this.elem = elem)} onClick={this.onScrollToTop}>
+        <img src={button} alt="scroll To Top" />
+      </div>
+    );
+  }
+}
