0dd70574b6059ed50096d2492880abb0ee9f953d
Martyn Janes <martyn@obany.com>
2018-12-13 11:44:59 +0300

    Added pagination molecule

---
+++ src/components/molecules/Pagination/index.js
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class Pagination extends React.Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    onChange: PropTypes.func
+  };
+
+  constructor(props) {
+    super(props)
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(newIndex) {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(newIndex);
+    }
+  }
+
+  render() {
+    return (
+      <ul class="pagination">
+        ({
+          Array.from(Array(this.props.total).keys()).map(p => (
+            <li className={
+              classNames(
+                'pagination-item',
+                { 'pagination-item--selected': (p + 1) === this.props.value }
+              )
+            }><a onClick={() => this.handleOnChange(p + 1)}>{p + 1}</a></li>
+          ))
+        })
+      </ul>
+    )
+  }
+}
+
+export default Pagination;
