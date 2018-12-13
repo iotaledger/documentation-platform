c51a0702137c92d8b07fe7cba80060b8454b0d7e
Martyn Janes <martyn@obany.com>
2018-12-13 12:37:18 +0300

    Added horizontal rule atom

---
+++ src/components/atoms/HorizontalRule/index.js
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// <HorizontalRule />
+// <HorizontalRule small />
+
+class HorizontalRule extends React.Component {
+  static propTypes = {
+    small: PropTypes.bool
+  };
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <hr
+        className={
+          classNames(
+            { 'horizontal-rule--small': this.props.small }
+          )
+        } />
+    )
+  }
+}
+
+export default HorizontalRule;
