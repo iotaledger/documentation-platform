9f9109430e80be62c36dc36d6762409d341b9068
mtrabelsi <marwen.projob@gmail.com>
2018-12-14 16:09:07 +0100

    added Link for navigation to search

--- src/components/molecules/Result/index.js
+++ src/components/molecules/Result/index.js
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Text from './../../atoms/Text'
+import { Link } from 'react-static'
 
 class Result extends React.Component {
   constructor(props) {
@@ -15,7 +16,11 @@ class Result extends React.Component {
         {this.props.foundResult.map(elm => {
           return (<div key={elm.id} style={{}}>
             <section><Text className='text text--level3 text--tertiary'>{elm.name}</Text></section>
-            <section><Text className='text-paragraph'>{elm.id}</Text></section>
+            <section>
+              <Link to={elm.id} exact>
+                <Text className='text-paragraph'>{elm.id}</Text>
+              </Link>
+            </section>
           </div>)
         })}
       </section>
