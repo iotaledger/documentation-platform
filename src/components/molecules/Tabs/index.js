27595c4a46b790a428652aa8382d99bb4ea69a26
Martyn Janes <martyn@obany.com>
2018-12-17 10:42:55 +0300

    Added favicon

--- src/components/molecules/Tabs/index.js
+++ src/components/molecules/Tabs/index.js
@@ -61,10 +61,10 @@ class Tabs extends React.Component {
           </ul>
           <div className="tab-header-copy">
             <span className={classNames(
-                  "tab-header-copy-text",
-                  { "tab-header-copy-text--active": this.state.copyActive },
-                  { "tab-header-copy-text--failed": !this.state.copySuccess }
-                )}>{this.state.copySuccess ? "Copied" : "Failed"}</span>
+              "tab-header-copy-text",
+              { "tab-header-copy-text--active": this.state.copyActive },
+              { "tab-header-copy-text--failed": !this.state.copySuccess && this.state.copyActive }
+            )}>{this.state.copySuccess ? "Copied" : "Failed"}</span>
             <button className="tab-header-copy-button" onClick={() => this.handleCopy()}><i className="fa fa-copy"></i></button>
           </div>
         </div>
--- static.config.js
+++ static.config.js
@@ -78,7 +78,7 @@ export default {
     }
   ],
   renderToHtml: (render, Comp, meta) => {
-    try{
+    try {
       const sheet = new ServerStyleSheet();
       const html = render(sheet.collectStyles(<Comp />));
       meta.styleTags = sheet.getStyleElement();
@@ -99,6 +99,15 @@ export default {
               name="viewport"
               content="width=device-width, initial-scale=1"
             />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <meta name="apple-mobile-web-app-title" content={repoName} />
+            <meta name="application-name" content={repoName} />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
             {renderMeta.styleTags}
             <title>{repoName}</title>
           </Head>
