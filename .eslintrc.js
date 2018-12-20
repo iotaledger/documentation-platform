67274cc094f8bac166eac0b4ea9462ff0e912505
Martyn Janes <martyn@obany.com>
2018-12-20 08:33:57 +0300

    Added eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,36 @@
 module.exports = {
-  extends: 'react-tools'
-}
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
