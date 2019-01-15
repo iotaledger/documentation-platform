e0f9cfd924d992537448bc5c523d8f2748a72de9
Martyn Janes <martyn@obany.com>
2019-01-15 12:48:52 +0300

    Remove lag in scroll container

--- src/components/atoms/ScrollInContainer.js
+++ src/components/atoms/ScrollInContainer.js
@@ -46,45 +46,42 @@ class ScrollInContainer extends React.Component {
             return;
         }
 
-        // Do the update on next time slice to debounce it otherwise it doesn't always get initial position correct
-        setTimeout(() => {
-            const topMarker = document.querySelectorAll(this.props.topMarker);
-            const bottomMarker = document.querySelectorAll(this.props.bottomMarker);
+        const topMarker = document.querySelectorAll(this.props.topMarker);
+        const bottomMarker = document.querySelectorAll(this.props.bottomMarker);
 
-            const thisDom = ReactDOM.findDOMNode(this);
-            const docDom = ReactDOM.findDOMNode(document.body);
+        const thisDom = ReactDOM.findDOMNode(this);
+        const docDom = ReactDOM.findDOMNode(document.body);
 
-            const thisRect = thisDom.getBoundingClientRect();
+        const thisRect = thisDom.getBoundingClientRect();
 
-            const topDom = topMarker && topMarker.length ? ReactDOM.findDOMNode(topMarker[0]) : docDom;
-            const bottomDom = bottomMarker && bottomMarker.length ? ReactDOM.findDOMNode(bottomMarker[0]) : docDom;
+        const topDom = topMarker && topMarker.length ? ReactDOM.findDOMNode(topMarker[0]) : docDom;
+        const bottomDom = bottomMarker && bottomMarker.length ? ReactDOM.findDOMNode(bottomMarker[0]) : docDom;
 
-            let topLimit = topDom.getBoundingClientRect().bottom;
-            let bottomLimit = bottomDom.getBoundingClientRect().top;
+        let topLimit = topDom.getBoundingClientRect().bottom;
+        let bottomLimit = bottomDom.getBoundingClientRect().top;
 
-            let newTop = topLimit + this.props.topOffset;
-            if (newTop < this.props.topOffset) {
-                newTop = this.props.topOffset;
-            }
+        let newTop = topLimit + this.props.topOffset;
+        if (newTop < this.props.topOffset) {
+            newTop = this.props.topOffset;
+        }
 
-            if (newTop + thisRect.height > (bottomLimit - this.props.bottomOffset)) {
-                newTop = bottomLimit - this.props.bottomOffset - thisRect.height;
-            }
+        if (newTop + thisRect.height > (bottomLimit - this.props.bottomOffset)) {
+            newTop = bottomLimit - this.props.bottomOffset - thisRect.height;
+        }
 
-            const widthContainer = document.querySelectorAll(this.props.widthContainer);
-            const newWidthContainer = widthContainer && widthContainer.length > 0 ?
-                ReactDOM.findDOMNode(widthContainer[0]) : undefined;
+        const widthContainer = document.querySelectorAll(this.props.widthContainer);
+        const newWidthContainer = widthContainer && widthContainer.length > 0 ?
+            ReactDOM.findDOMNode(widthContainer[0]) : undefined;
 
-            if (newWidthContainer) {
-                const widthContainerComputedStyle = getComputedStyle(newWidthContainer);
-                const newWidth = newWidthContainer.getBoundingClientRect().width
-                    - parseInt(widthContainerComputedStyle.paddingLeft)
-                    - parseInt(widthContainerComputedStyle.paddingRight);
-                this.container.style.width = `${newWidth}px`;
-            }
-            this.container.style.top = `${newTop}px`;
-            this.container.style.display = 'block';
-        }, 0);
+        if (newWidthContainer) {
+            const widthContainerComputedStyle = getComputedStyle(newWidthContainer);
+            const newWidth = newWidthContainer.getBoundingClientRect().width
+                - parseInt(widthContainerComputedStyle.paddingLeft)
+                - parseInt(widthContainerComputedStyle.paddingRight);
+            this.container.style.width = `${newWidth}px`;
+        }
+        this.container.style.top = `${newTop}px`;
+        this.container.style.display = 'block';
     }
 
     render() {
