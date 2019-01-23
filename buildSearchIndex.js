5d2293c95b4a6aaff165047c46c1c0e64b655b00
Lewis Freiberg <lewi@2pk.in>
2019-01-23 16:54:54 +0100

    Strip the indexer down

--- buildSearchIndex.js
+++ buildSearchIndex.js
@@ -1,85 +1,70 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const cheerio = require('cheerio');
-const lunr = require('lunr');
+const fs = require('fs')
+const path = require('path')
+const cheerio = require('cheerio')
 
-const dir = process.argv[2];
-const indexFile = process.argv[3];
-const corpusFile = process.argv[4];
+const dir = process.argv[2]
+const corpusFile = process.argv[3]
 
-const webifyPath = (p) => p.replace(/\\/g, '/');
+const webifyPath = p => p.replace(/\\/g, '/')
 
 if (!dir) {
-    console.log('\nUsage: \ndirectory\n');
-    process.exit(1);
+  console.log('\nUsage: \ndirectory\n')
+  process.exit(1)
 }
 
-const listFiles = dir => fs.statSync(dir).isDirectory()
-    ? Array.prototype.concat(...fs.readdirSync(dir).map(f => listFiles(path.join(dir, f))))
-    : dir;
+const listFiles = dir =>
+  fs.statSync(dir).isDirectory()
+    ? Array.prototype.concat(
+        ...fs.readdirSync(dir).map(f => listFiles(path.join(dir, f)))
+      )
+    : dir
 
-const files = listFiles(dir);
-const corpus = [];
+const files = listFiles(dir)
+const corpus = []
 
 const indexDocs = callback => {
-    // setup lunr
-    const lunrIndex = lunr(function () {
-        this.ref('id');
-        this.field('docTitle', { boost: 10 });
-        this.field('docBody', { boost: 5 });
+  files
+    .filter(f => /.md$/i.test(f))
+    .forEach((fileLocation, index) => {
+      const file = fs.readFileSync(fileLocation, 'utf8')
+      const md = require('markdown-it')()
+      const renderedHtml = md.render(file)
+      const $ = cheerio.load(renderedHtml)
 
-        files.filter(f => /.md$/i.test(f)).forEach((fileLocation, index) => {
-            const file = fs.readFileSync(fileLocation, 'utf8');
-            const md = require('markdown-it')();
-            const renderedHtml = md.render(file);
-            const $ = cheerio.load(renderedHtml);
+      const docName = webifyPath(fileLocation)
+        .match(/[^\/]+$/)[0]
+        .replace('.md', '')
 
-            const docName = webifyPath(fileLocation).match(/[^\/]+$/)[0].replace('.md', '');
+      let docTitle = $('h1')
+        .first()
+        .text()
+      let docSummary = $('p')
+        .first()
+        .text()
+        .substr(0, 160)
 
-            let docTitle = $('h1').first().text();
-            let docSummary = $('p').first().text().substr(0, 160);
+      // set the docTitle
+      if (docTitle.trim() === '') {
+        docTitle = docName
+      }
 
-            // set the docTitle
-            if (docTitle.trim() === '') {
-                docTitle = docName;
-            }
+      corpus.push({
+        id: webifyPath(fileLocation).replace('.md', ''),
+        name: docTitle,
+        summary: docSummary
+      })
+    })
 
-            corpus.push({
-                id: webifyPath(fileLocation).replace('.md', ''),
-                name: docTitle,
-                summary: docSummary
-            });
+  const dirName = path.dirname(corpusFile)
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName)
+  }
 
-            const indexDoc = {
-                docTitle,
-                docBody: $.html(),
-                id: webifyPath(fileLocation).replace('.md', '')
-            };
+  fs.writeFileSync(corpusFile, JSON.stringify(corpus), 'utf8', err =>
+    err ? console.error(err) : null
+  )
+}
 
-            this.add(indexDoc);
-        }, this);
-    });
-    callback(lunrIndex);
-};
-
-indexDocs(lunrIndex => {
-    if (indexFile) {
-        const dirName = path.dirname(indexFile);
-        if (!fs.existsSync(dirName)) {
-            fs.mkdirSync(dirName);
-        }
-        
-        fs.writeFileSync(indexFile, JSON.stringify(lunrIndex), 'utf8', err => (err ? console.error(err) : null));
-    }
-
-    if (corpusFile) {
-        const dirName = path.dirname(corpusFile);
-        if (!fs.existsSync(dirName)) {
-            fs.mkdirSync(dirName);
-        }
-
-        fs.writeFileSync(corpusFile, JSON.stringify(corpus), 'utf8', err => (err ? console.error(err) : null));
-    }
-});
+indexDocs()
