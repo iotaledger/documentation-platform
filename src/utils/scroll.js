22f36426f001e25de9e1c8961ef5fc25b0fd6237
Martyn Janes <martyn@obany.com>
2019-01-29 10:52:36 +0300

    Fix scrolling on safari

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,30 +1,18 @@
 export const scrollIntoView = (elem) => {
-    // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-    const isIE = /*@cc_on!@*/false || !!document.documentMode;
-    const isEdge = !isIE && !!window.StyleMedia;
-
-    const scrollObject = isIE || isEdge ? document.body : document.documentElement;
-
     const animate = (start, from, to, duration) => {
         const time = Math.min(1, ((Date.now() - start) / duration));
         const eased = 0.5 * (1 - Math.cos(Math.PI * time));
   
-        scrollObject.scrollTop = (eased * (to - from)) + from;
+        document.scrollingElement.scrollTop = (eased * (to - from)) + from;
 
         if (time < 1) {
             setTimeout(() => animate(start, from , to, duration), 0);
         }
     };
 
-    animate(Date.now(), scrollObject.scrollTop, elem.offsetTop, 1000);
+    animate(Date.now(), document.scrollingElement.scrollTop, elem.offsetTop, 1000);
 };
 
 export const currentScrollTop = () => {
-    // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-    const isIE = /*@cc_on!@*/false || !!document.documentMode;
-    const isEdge = !isIE && !!window.StyleMedia;
-
-    const scrollObject = isIE || isEdge ? document.body : document.documentElement;
-
-    return scrollObject.scrollTop;
+    return document.scrollingElement.scrollTop;
 };
