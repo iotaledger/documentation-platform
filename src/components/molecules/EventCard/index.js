04ccdd4d4dd8e603042a6685ae1f8da0f83b74df
Martyn Janes <martyn@obany.com>
2019-02-28 11:50:14 +0300

    Added feed and event card components

---
+++ src/components/molecules/EventCard/index.js
@@ -0,0 +1,91 @@
+import classnames from 'classnames';
+import GoogleMapReact from 'google-map-react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import logo from '../../../assets/logo-small.svg';
+import { sanitizeHashId } from '../../../utils/paths';
+import './eventCard.css';
+import EventCardMarker from './EventCardMarker';
+
+/**
+ * Component to display an event.
+ */
+class EventCard extends Component {
+    static propTypes = {
+        item: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            startUtc: PropTypes.string.isRequired,
+            urlDetails: PropTypes.string,
+            venueName: PropTypes.string,
+            venueAddress: PropTypes.arrayOf(PropTypes.string),
+            venueLatitude: PropTypes.number,
+            venueLongitude: PropTypes.number
+        }).isRequired,
+        googleMapsKey: PropTypes.string.isRequired
+
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showMap: false
+        };
+    }
+
+    render() {
+        return (
+            <div className="event-card">
+                <h2 id={sanitizeHashId(this.props.item.title)} className="event-card--title">{this.props.item.title}</h2>
+                <div className="event-card--date">{moment(this.props.item.startUtc).format('MMMM Do YYYY, h:mm a')}</div>
+                <div className="event-card--description">{this.props.item.description}</div>
+                <hr />
+                <div className="event-card--venue">
+                    {this.props.item.venueName && (
+                        <div className="event-card--venue-name">{this.props.item.venueName}</div>
+                    )}
+                    {this.props.item.venueAddress && this.props.item.venueAddress.map((a, idx) => (
+                        <div className="event-card--venue-address" key={idx}>{a}</div>
+                    ))}
+                </div>
+                <div className="event-card--actions">
+                    <a className="button button--secondary" href={this.props.item.urlDetails} target="_blank" role="button" rel="noreferrer noopener">More Details</a>
+                </div>
+                {this.props.item.venueLatitude && this.props.item.venueLongitude && (
+                    <div className="event-card--map-container">
+                        <a
+                            className="event-card--map-toggle icon-up-down"
+                            onClick={() => this.setState({ showMap: !this.state.showMap })}
+                            role="button"
+                        >{this.state.showMap ? 'Hide Map' : 'Show Map'}&nbsp;
+                        </a>
+
+                        <div
+                            className={classnames(
+                                'event-card--map',
+                                { 'event-card--map__hidden': !this.state.showMap }
+                            )}
+                        >
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: this.props.googleMapsKey }}
+                                defaultCenter={{
+                                    lat: this.props.item.venueLatitude,
+                                    lng: this.props.item.venueLongitude
+                                }}
+                                defaultZoom={14}
+                            >
+                                <EventCardMarker lat={this.props.item.venueLatitude} lng={this.props.item.venueLongitude}>
+                                    <img src={logo} alt="IOTA" />
+                                    {this.props.item.venueName}
+                                </EventCardMarker>
+                            </GoogleMapReact>
+                        </div>
+                    </div>
+                )}
+            </div>);
+    }
+}
+
+export default EventCard;
---
+++ src/components/organisms/Feed/index.js
@@ -0,0 +1,133 @@
+import qs from 'qs';
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import { feed } from '../../../utils/api';
+import EventCard from '../../molecules/EventCard';
+import Pagination from '../../molecules/Pagination';
+import { scrollIntoView } from '../../../utils/scroll';
+
+class Feed extends React.Component {
+    static propTypes = {
+        apiEndpoint: PropTypes.string.isRequired,
+        googleMapsKey: PropTypes.string,
+        displayType: PropTypes.string.isRequired,
+        context: PropTypes.string.isRequired,
+        onLoaded: PropTypes.func,
+        history: ReactRouterPropTypes.history
+    };
+
+    constructor(props) {
+        super(props);
+
+        let paramPage = 0;
+        if (props.history.location.search) {
+            const params = qs.parse(props.history.location.search, { ignoreQueryPrefix: true });
+            if (params && params.page) {
+                paramPage = parseInt(params.page, 10) - 1;
+            }
+        }
+
+        this.state = {
+            status: '',
+            items: [],
+            initialHash: props.history.location.hash,
+            page: paramPage,
+            pageSize: 2,
+            totalPages: 0,
+            totalItems: 0
+        };
+
+        this.onDataPaginated = this.onDataPaginated.bind(this);
+        this.loadData = this.loadData.bind(this);
+    }
+
+    async componentDidMount() {
+        await this.loadData();
+    }
+
+    loadData() {
+        const target = document.querySelector('#root');
+        if (target) {
+            scrollIntoView(target);
+        }
+
+        this.setState({ 
+            status: 'Retreiving, please wait...',
+            items: [],
+            totalPages: 0,
+            totalItems: 0
+        }, async () => {
+            if (this.props.onLoaded) {
+                this.props.onLoaded(0);
+            }
+            this.updatePageUrl();
+
+            const response = await feed(this.props.apiEndpoint, this.props.context, this.state.page, this.state.pageSize);
+
+            if (response.success) {
+                this.setState({
+                    items: response.items,
+                    status: '',
+                    totalPages: response.totalPages,
+                    totalItems: response.totalItems
+                }, () => {
+                    if (this.props.onLoaded) {
+                        this.props.onLoaded(response.totalItems);
+                    }
+                    this.updatePageUrl();
+                    if (this.state.initialHash) {
+                        const target = document.querySelector(this.state.initialHash);
+                        if (target) {
+                            scrollIntoView(target);
+                        }
+                    }
+                });
+            } else {
+                this.setState({
+                    items: [],
+                    status: 'Failed loading feed',
+                    totalPages: 0,
+                    totalItems: 0
+                });
+            }
+        });
+    }
+
+    onDataPaginated(pageIndex) {
+        this.setState({
+            initialHash: '',
+            page: pageIndex
+        }, async () => {
+            await this.loadData();
+        });
+    }
+
+    updatePageUrl() {
+        let url = this.props.history.location.pathname;
+        if (this.state.page > 0) {
+            url += `?page=${this.state.page + 1}`;
+        }
+        if (this.state.initialHash) {
+            url += this.state.initialHash;
+        }
+
+        this.props.history.replace(url);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.status}
+                {this.state.items.map((item, idx) =>
+                    this.props.displayType === 'events' && <EventCard item={item} key={idx} googleMapsKey={this.props.googleMapsKey} />
+                )}
+                {this.state.totalPages > 0 && (
+                    <Pagination totalCount={this.state.totalItems} page={this.state.page} maxPerPage={this.state.pageSize} onDataPaginated={this.onDataPaginated} />
+                )}
+            </React.Fragment>
+        );
+    }
+}
+
+export default Feed;
