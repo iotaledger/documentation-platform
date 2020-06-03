9bb3e80bcd4cc1d1304b2bdf8c55eb587b022bb5
Martyn Janes <martyn@obany.com>
2020-06-03 16:46:22 +0100

    Update services

--- api/src/initServices.ts
+++ api/src/initServices.ts
@@ -5,4 +5,5 @@ import { IConfiguration } from "./models/configuration/IConfiguration";
  * @param config The configuration to initialisation the service with.
  */
 export async function initServices(config: IConfiguration): Promise<void> {
+    // No services for this project
 }
