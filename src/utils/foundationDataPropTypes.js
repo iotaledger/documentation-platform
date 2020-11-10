f8ec7506ea94e9f041d4e05272d9c052923e985b
Martyn Janes <martyn@obany.com>
2020-11-10 09:00:46 +0100

    Fix footer proptype definitions

--- src/utils/foundationDataPropTypes.js
+++ src/utils/foundationDataPropTypes.js
@@ -6,13 +6,8 @@ const FoundationDataPropTypes = PropTypes.shape({
             label: PropTypes.string.isRequired,
             items: PropTypes.arrayOf(
                 PropTypes.shape({
-                    heading: PropTypes.string.isRequired,
-                    links: PropTypes.arrayOf(
-                        PropTypes.shape({
-                            link: PropTypes.string.isRequired,
-                            name: PropTypes.string.isRequired
-                        })
-                    ).isRequired
+                    label: PropTypes.string.isRequired,
+                    url: PropTypes.string.isRequired
                 })
             ).isRequired
         })
@@ -25,16 +20,18 @@ const FoundationDataPropTypes = PropTypes.shape({
         label: PropTypes.string.isRequired,
         value: PropTypes.arrayOf(PropTypes.string).isRequired
     }),
-    information: PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        value: PropTypes.string,
-        urls: PropTypes.arrayOf(
-            PropTypes.shape({
-                label: PropTypes.string.isRequired,
-                url: PropTypes.string.isRequired
-            })
-        )
-    }),
+    information: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            value: PropTypes.string,
+            urls: PropTypes.arrayOf(
+                PropTypes.shape({
+                    label: PropTypes.string.isRequired,
+                    url: PropTypes.string.isRequired
+                })
+            )
+        })
+    ),
 });
 
 export default FoundationDataPropTypes;
